72f6c7df4533c64d933abf85bda64fa9cce087c6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2020-05-04 23:27:47 +0900

    ソースコード修正、テンプレートタグの追加
    wpのテンプレートタグ、現在有効化されているテーマディレクトリのURLを返す

--- html/index.php
+++ html/index.php
@@ -6,12 +6,12 @@
   <meta name="keywords" content="共通キーワード" />
   <meta name="description" content="共通ディスクリプション" />
   <title>PACIFIC MALL DEVELOPMENT</title>
-  <link rel="shortcut icon" href="./assets/images/common/favicon.ico" />
+  <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/assets/images/common/favicon.ico" />
   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/styles.css" />
-  <script type="text/javascript" src="./assets/js/jquery-3.3.1.min.js"></script>
-  <script type="text/javascript" src="./assets/js/bundle.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri(); ?>/assets/css/styles.css" />
+  <script type="text/javascript" src="<?= get_template_directory_uri(); ?>/assets/js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="<?= get_template_directory_uri(); ?>/assets/js/bundle.js"></script>
 </head>
 <body>
   <div class="container">
